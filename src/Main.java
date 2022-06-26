83e607975bd1d42dca05f3cb91b9f412520a6fbd
cami-cohete <esquivca@mail.uc.edu>
2022-06-26 19:48:14 -0400

    Part 1B of project.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main
 
     public static void main(String[] args)
     {
+        // Part A of project. For loops
         int cnt = 0;
         for (cnt = 0; cnt <=30; cnt++)
         {
@@ -30,5 +31,18 @@ public class Main
             System.out.printf("%3d", cnt);
         }
         System.out.println("");
+
+        // Part B of project. Nested Loops
+        final int ROW = 5;
+        final int COL = 5;
+
+        for (int row = 0; row <= ROW; row++)
+        {
+            for (int col = row; col >= 0; col--)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
