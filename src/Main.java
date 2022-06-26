d7d42c0daf64788526a057a150a5d61aec88db8b
cami-cohete <esquivca@mail.uc.edu>
2022-06-26 19:05:55 -0400

    Part 1A of project.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        int cnt = 0;
+        for (cnt = 0; cnt <=30; cnt++)
+        {
+            System.out.printf("%3d", cnt);
+        }
+
     }
 }
