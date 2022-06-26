8fab3eab33f1d70c46b249e5c7d3974763c9c922
cami-cohete <esquivca@mail.uc.edu>
2022-06-26 19:20:51 -0400

    Part 4A of project.

--- src/Main.java
+++ src/Main.java
@@ -18,11 +18,17 @@ public class Main
         System.out.println("");
 
 
-        for (cnt = 0; cnt <=18; cnt = cnt = cnt + 3)
+        for (cnt = 0; cnt <=18; cnt = cnt + 3)
         {
             System.out.printf("%3d", cnt);
         }
         System.out.println("");
 
+
+        for (cnt = 10; cnt >= 0; cnt = cnt - 2)
+        {
+            System.out.printf("%3d", cnt);
+        }
+        System.out.println("");
     }
 }
